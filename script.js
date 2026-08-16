c8f8a79f16edd78fe7a1a8b31e0f320f9cbac67a
SID AHMED HAMAD <151679970+sidi1999@users.noreply.github.com>
2026-08-16 23:47:45 +0200

    changes in readme and js

--- script.js
+++ script.js
@@ -3,25 +3,86 @@
 // 3. con una funcion calcular el valor de la cuenta y el tip por persona 
 // 4. insertar la info en el apartado de los amount
 
-
+// variables
 const contInput1 = document.querySelector("#cont-input1")
 const contInput2 = document.querySelector("#cont-input2")
 const inputBill = document.getElementById("input-bill");
 const inputPeople = document.getElementById("input-people");
 const resetBtn = document.querySelector("#btn-reset");
-
-// Seleccionar elementos del resultado ANTES de usarlos
 const tip = document.getElementById("tip");
 const totalBill = document.getElementById("total-bill");
 
 const btns = document.querySelectorAll(".btn");
 const error = document.querySelector(".error");
-
-error.classList.add("hidden");
 let bill = 0;
 let people = 0;
 let btnSelected = 0;
 
+
+
+
+
+
+
+
+// input bill
+inputBill.addEventListener('input', (e)=> 
+    { bill = parseFloat(e.target.value)|| 0;
+    if(bill > 0){
+        contInput1.classList.add("border-green");
+        contInput1.classList.remove("border-red");
+        calculatorTip();}
+    else{
+        contInput1.classList.add("border-red");
+        contInput1.classList.remove("border-green");
+    }
+    console.log(bill);
+})
+// btns
+btns.forEach((btn)=>{
+    btn.addEventListener('click', (event)=>{
+        btns.forEach(b => b.classList.remove("btn-active"));
+        btnSelected = parseFloat(event.target.value);
+        btn.classList.add("btn-active");
+        calculatorTip();
+    })
+})
+
+// input people
+inputPeople.addEventListener('input', (e)=> {people = parseFloat(e.target.value);
+    calculatorTip();
+})
+   
+
+// function 
+const calculatorTip = ()=>{
+    if (people > 0 ){
+    contInput2.classList.add("border-green")
+    const billperson = bill / people;
+    const tipCalc = (billperson * btnSelected)/100;
+
+    const billtotal = billperson + tipCalc;
+
+    tip.textContent = `$${tipCalc.toFixed(2)}`;
+    totalBill.textContent = `$${billtotal.toFixed(2)}`;
+    error.classList.add("hidden");
+
+    contInput2.classList.add("border-green");
+    contInput2.classList.remove("border-red");
+    resetBtn.disabled= false;
+    
+    }
+    else{
+        error.classList.remove("hidden");
+        contInput2.classList.add("border-red");
+        contInput2.classList.remove("border-green");
+    }
+    
+}
+
+error.classList.add("hidden");
+
+resetBtn.disabled= true;
 resetBtn.addEventListener('click', ()=>{
     bill = 0;
     people = 0;
@@ -33,49 +94,6 @@ resetBtn.addEventListener('click', ()=>{
     // Limpiar los bordes
     contInput1.classList.remove("border-red", "border-green");
     contInput2.classList.remove("border-red", "border-green");
+    btns.forEach(b => b.classList.remove("btn-active"));
+    resetBtn.disabled=true;
 })
-
-
-inputBill.addEventListener('input', (e)=> {bill = parseFloat(e.target.value) || 0;
-    if(bill > 0){
-        contInput1.classList.add("border-green")
-        calculatorTip();
-    }
-
-    else{
-        contInput1.classList.add("border-red")
-    }
-    
-})
-inputPeople.addEventListener('input', (e)=> {people = parseFloat(e.target.value) || 0;
-    calculatorTip();
-})
-   
-btns.forEach((btn)=>{
-    btn.addEventListener('click', (event)=>{
-        btns.forEach(b => b.classList.remove("btn-active"));
-        btnSelected = parseFloat(event.target.value) || 0;
-        btn.classList.add("btn-active");
-        calculatorTip();
-    })
-})
-
-const calculatorTip = ()=>{
-    if (people > 0 ){
-    contInput2.classList.add("border-green")
-    const billperoson = bill / people
-    const tipCalc = (billperoson * btnSelected)/100;
-
-    const billtotal = billperoson + tipCalc;
-
-    tip.textContent = `$${tipCalc.toFixed(2)}`;
-    totalBill.textContent = `$${billtotal.toFixed(2)}`;
-    error.classList.add("hidden");
-    }
-    else{
-        error.classList.remove("hidden");
-        contInput2.classList.add("border-red")
-    }
-    
-}
-
